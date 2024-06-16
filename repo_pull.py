ccc45a7769e3beb15cfe74e1c9da47bd08139c43
Adnaan Khan <adnaankhan@Adnaans-MacBook-Air.local>
2024-06-16 17:09:06 -0400

    Python script added to pull code

---
+++ repo_pull.py
@@ -0,0 +1,42 @@
+import paramiko
+from git import Repo
+
+#Configuration
+git_repo_url = 'https://github.com/adukhan98/cicd-demo/tree/main'
+local_repo_dir = '/path/to/local/repo'
+remote_server_ip = ''
+ssh_user = ''
+ssh_password = '' # For key-based auth, use key_filename parameter instead
+remote_project_dir = ''
+server_restart_command = 'sudo systemctl restart your-web-service'
+
+#Clone or pull the latest code from the repository
+repo = Repo(local_repo_dir)
+if repo.bare:
+    print("Cloning repository...")
+    repo = Repo.clone_from(git_repo_url, local_repo_dir)
+else:
+    print("Pulling latest code...")
+    repo.git.pull()
+
+#SSH connection
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(remote_server_ip, username=ssh_user, password=ssh_password)
+
+#Deployment commands
+commands = [
+f'cd {remote_project_dir}',
+'git pull',
+'pip install -r requirements.txt', # If your project has a requirements.txt
+server_restart_command
+]
+
+#Execute commands
+for command in commands:
+    stdin, stdout, stderr = ssh.exec_command(command)
+    print(stdout.read().decode())
+    print(stderr.read().decode())
+
+ssh.close()
+print("Deployment completed.")
